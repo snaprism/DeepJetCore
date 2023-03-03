418cdb3085183f9a27f2613211cd71d44a64ef7d
P. A. Jung <alexander.jung@rwth-aachen.de>
2023-03-03 10:29:50 +0100

    more robust docker env

--- docker_env.sh
+++ docker_env.sh
@@ -1,9 +1,9 @@
-export DEEPJETCORE=`pwd`
-export PATH=`pwd`/bin:$PATH
-export PYTHONPATH=`pwd`/../:$PYTHONPATH
+export DEEPJETCORE=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+export PATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")/bin:$PATH
+export PYTHONPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")/../:$PYTHONPATH
 if [ $LD_LIBRARY_PATH ]
 then
-    export LD_LIBRARY_PATH=`pwd`/compiled/:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")/compiled/:$LD_LIBRARY_PATH
 else
-    export LD_LIBRARY_PATH=`pwd`/compiled/
+    export LD_LIBRARY_PATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")/compiled/
 fi
